a6620b539cccf56d64d03fae5899ff12025d6293
KMC-Workshop2024 <190717282+KMC-Workshop2024@users.noreply.github.com>
2026-09-11 05:25:22 +1000

    test: cover private FSD reconnect and packet fragmentation

--- client/tests/test_fsd_private.cpp
+++ client/tests/test_fsd_private.cpp
@@ -42,6 +42,59 @@ private slots:
         QTest::qWait(100);
         QCOMPARE(peer->bytesAvailable(), 0);
     }
+
+    void fragmentedVelocityPacketIsParsedOnce()
+    {
+        QTcpServer server;
+        QVERIFY(server.listen(QHostAddress::LocalHost));
+
+        FsdClient client;
+        int velocityPackets = 0;
+        QString callsign;
+        connect(&client, &FsdClient::RaiseFastPilotPositionReceived,
+                this, [&](const PDUFastPilotPosition& pdu) {
+                    ++velocityPackets;
+                    callsign = pdu.From;
+                });
+
+        client.Connect("127.0.0.1", server.serverPort(), false);
+        QTRY_VERIFY_WITH_TIMEOUT(server.hasPendingConnections(), 1000);
+        QTcpSocket* peer = server.nextPendingConnection();
+
+        peer->write("^KMA002:-37.1:145.2:10000:9000:12582828:100.0:0.0");
+        peer->flush();
+        QTest::qWait(50);
+        QCOMPARE(velocityPackets, 0);
+
+        peer->write(":0.0:0.0:0.0:0.0:0.0:0.0\r\n");
+        peer->flush();
+        QTRY_COMPARE_WITH_TIMEOUT(velocityPackets, 1, 1000);
+        QCOMPARE(callsign, QString("KMA002"));
+    }
+
+    void privateConnectionCanReconnectCleanly()
+    {
+        QTcpServer server;
+        QVERIFY(server.listen(QHostAddress::LocalHost));
+
+        FsdClient client;
+        client.Connect("127.0.0.1", server.serverPort(), false);
+        QTRY_VERIFY_WITH_TIMEOUT(server.hasPendingConnections(), 1000);
+        QTcpSocket* firstPeer = server.nextPendingConnection();
+        QVERIFY(firstPeer != nullptr);
+
+        firstPeer->write("^INCOMPLETE");
+        firstPeer->flush();
+        QTest::qWait(20);
+        client.Disconnect();
+        QTRY_VERIFY_WITH_TIMEOUT(!client.IsConnected(), 1000);
+
+        client.Connect("127.0.0.1", server.serverPort(), false);
+        QTRY_VERIFY_WITH_TIMEOUT(server.hasPendingConnections(), 1000);
+        QTcpSocket* secondPeer = server.nextPendingConnection();
+        QVERIFY(secondPeer != nullptr);
+        QTRY_VERIFY_WITH_TIMEOUT(client.IsConnected(), 1000);
+    }
 };
 
 QTEST_MAIN(TestPrivateFsd)
